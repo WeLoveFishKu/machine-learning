00d0d163e59c2ff53ec40b338d80802f592d3b4d
bagood <4riabagus@gmail.com>
2023-06-04 13:20:25 +0700

    update the program to be connected to an api

--- ImageValidation/Execute.py
+++ ImageValidation/Execute.py
@@ -7,15 +7,8 @@ train_set, validation_set = validatefishimage_functions.load_data()
 
 # Develop models
 learning_rate = 0.001
-epochs = 10
+epochs = 20
 model = validatefishimage_functions.create_model()
 model = validatefishimage_functions.model_fitting(model, learning_rate, epochs, train_set, validation_set)
 
-# Create prediction
-ikan1 = '/Users/ariabagus/Desktop/Coding_Duniawi/Code/machine-learning/ImageValidation/TestImages/ikan1.jpeg'
-ikan2 = '/Users/ariabagus/Desktop/Coding_Duniawi/Code/machine-learning/ImageValidation/TestImages/ikan2.jpeg'
-bukanikan1 = '/Users/ariabagus/Desktop/Coding_Duniawi/Code/machine-learning/ImageValidation/TestImages/bukanikan1.png'
-
-print(validatefishimage_functions.validate_fish_image(model, ikan1))
-print(validatefishimage_functions.validate_fish_image(model, ikan2))
-print(validatefishimage_functions.validate_fish_image(model, bukanikan1))
+model.save('modelValidateFishImage.h5')
---
+++ ImageValidation/apis.py
@@ -0,0 +1,26 @@
+from flask import Flask, request, jsonify
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+app = Flask(__name__)
+model = load_model('modelValidateFishImage.h5')
+
+@app.route('/validate', methods=['POST'])
+def validate_image():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded.'}), 400
+    
+    image_file = request.files['image']
+    img = image.load_img(image_file, target_size=(160, 160))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    images = np.vstack([img_array])
+    
+    prediction = model.predict(images, batch_size=32)
+    label = 'Fish' if prediction < 0.4 else 'Not a fish'
+    
+    return jsonify({'prediction': label})
+
+if __name__ == '__main__':
+    app.run()
