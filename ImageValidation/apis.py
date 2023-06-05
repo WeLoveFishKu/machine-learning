c9dde5e4b1c4707dc547443c9d37b9622074e8bf
bagood <4riabagus@gmail.com>
2023-06-05 22:22:37 +0700

    post response image

--- ImageValidation/apis.py
+++ ImageValidation/apis.py
@@ -1,5 +1,6 @@
 import os 
 import numpy as np
+import requests
 from flask import Flask, request, jsonify
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
@@ -7,17 +8,32 @@ from tensorflow.keras.preprocessing import image
 app = Flask(__name__)
 model = load_model('modelValidateFishImage.h5')
 
+def download_image(url, output_file):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+        with open(output_file, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+    except:
+        pass
+    return
+
 @app.route('/validate', methods=['POST'])
 def validate_image():
-    if 'image' not in request.files:
+    if 'photo_url' not in request.files:
         return jsonify({'error': 'No image uploaded.'}), 400
     
-    image_file = request.files['image']
-    img = image.load_img(image_file, target_size=(160, 160))
+    url = request.files.get('photo_url')
+    
+    if not url:
+        return
+    
+    download_image(url, 'current_img.jpg')
+    img = image.load_img('current_img.jpg', target_size=(160, 160))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     images = np.vstack([img_array])
-    
     prediction = model.predict(images, batch_size=32)
     label = 'Fish' if prediction < 0.4 else 'Not a fish'
     
